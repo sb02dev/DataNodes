75906a329d2ccfd518386dc0fe30b527ba4654df
sb02dev <sb02dev@gmail.coim>
2025-09-30 18:20:00 +0200

    added sqlite tests to test suite

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -6,3 +6,9 @@ from tests import testhelper  # pylint: disable=import-error
 def test_conn(pycnv, testfolder, test_name):
     """Tests all graphs from the parameters"""
     testhelper.run_export_and_compare(pycnv, testfolder, "conn_"+test_name)
+
+
+@pytest.mark.parametrize("test_name", testhelper.get_test_names('sqlite'))
+def test_sqlite(pycnv, testfolder, test_name):
+    """Tests all graphs from the parameters"""
+    testhelper.run_export_and_compare(pycnv, testfolder, "sqlite_"+test_name)
